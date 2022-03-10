f2d1ad493960a1bf3a7847281a08cb60ca2962a0
afterparty <37808129+afterparty7@users.noreply.github.com>
2022-03-10 16:55:35 +0200

    Delete 7.php

--- 7.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//Set Refresh header using PHP.
-header( "refresh:5;url=https://docs.google.com/forms/d/e/1FAIpQLScfB7UhxYizUaPpxebZIvsiVy7UHg2sYWCNAu6TwXCtePVoiQ/viewform" );
-
-//Print out some content for example purposes.
-echo 'This is the content.';
